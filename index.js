4a38a32c7fd17e118277ad190b89838f3eadbf81
Suganesan-T <suganesan71@gmail.com>
2024-06-07 20:49:38 +0530

    updated reset password

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
